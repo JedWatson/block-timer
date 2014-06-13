d6ff35ad6a764ca08b82454e5f1fff0d2c30c18a
Jed Watson <jed.watson@me.com>
2014-06-13 11:16:40 +1000

    Adding Stub and cleaning up formatting

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  */
 
 function each(obj, iterator, context) {
-    if (obj.forEach) {
-        obj.forEach(iterator, context);
-    } else {
-        var keys = Object.keys(obj);
-        for (var i = 0; i < keys.length; i++) {
-            iterator.call(context, obj[keys[i]], keys[i], obj);
-        }
-    }
-    return obj;
+	if (obj.forEach) {
+		obj.forEach(iterator, context);
+	} else {
+		var keys = Object.keys(obj);
+		for (var i = 0; i < keys.length; i++) {
+			iterator.call(context, obj[keys[i]], keys[i], obj);
+		}
+	}
+	return obj;
 };
 
 
@@ -20,20 +20,20 @@ function each(obj, iterator, context) {
  */
 
 function isObject(arg) {
-    return ('[object Object]' === Object.prototype.toString.call(arg));
+	return ('[object Object]' === Object.prototype.toString.call(arg));
 }
 
 function isArray(arg) {
-    return Array.isArray(arg);
+	return Array.isArray(arg);
 }
 
 function length(arg) {
-    return (isObject(arg) ? Object.keys(arg) : arg).length;
+	return (isObject(arg) ? Object.keys(arg) : arg).length;
 }
 
 function commas(str) {
-    str = '' + str;
-    return str.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+	str = '' + str;
+	return str.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
 
 
@@ -42,14 +42,14 @@ function commas(str) {
  */
 
 function newTimer() {
-    return {
-        start: Date.now()
-    };
+	return {
+		start: Date.now()
+	};
 }
 
 function endTimer(timer) {
-    timer.end = Date.now();
-    timer.delta = timer.end - timer.start;
+	timer.end = Date.now();
+	timer.delta = timer.end - timer.start;
 }
 
 
@@ -58,30 +58,30 @@ function endTimer(timer) {
  */
 
 function startById(timers, key, id) {
-
-    if (!timers[key]) {
-        timers[key] = {};
-    }
-
-    if (!isObject(timers[key])) {
-        throw new Error('Timer [' + key + '] initialised anonymously then called with an id.');
-    }
-
-    if (timers[key][id] && timers[key][id].end) {
-        throw new Error('Timer [' + key + '], id [' + id + '] started more than once.');
-    }
-
-    timers[key][id] = newTimer();
-
+	
+	if (!timers[key]) {
+		timers[key] = {};
+	}
+	
+	if (!isObject(timers[key])) {
+		throw new Error('Timer [' + key + '] initialised anonymously then called with an id.');
+	}
+	
+	if (timers[key][id] && timers[key][id].end) {
+		throw new Error('Timer [' + key + '], id [' + id + '] started more than once.');
+	}
+	
+	timers[key][id] = newTimer();
+	
 }
 
 function stopById(timers, key, id) {
-
-    if (!timers[key] || !timers[key][id] || timers[key][id].end) {
-        throw new Error('Timer [' + key + '], id [' + id + '] stopped more than once.');
-    }
-
-    endTimer(timers[key][id]);
+	
+	if (!timers[key] || !timers[key][id] || timers[key][id].end) {
+		throw new Error('Timer [' + key + '], id [' + id + '] stopped more than once.');
+	}
+	
+	endTimer(timers[key][id]);
 
 }
 
@@ -91,31 +91,31 @@ function stopById(timers, key, id) {
  */
 
 function startByIndex(timers, key) {
-
-    if (!timers[key]) {
-        timers[key] = [];
-    }
-
-    if (!isArray(timers[key])) {
-        throw new Error('Timer [' + key + '] initialised with an id then called anynomously.');
-    }
-
-    if (timers[key].length && timers[key][timers[key].length - 1].end) {
-        throw new Error('Timer [' + key + '] started again without being stopped.');
-    }
-
-    timers[key].push(newTimer());
-
+	
+	if (!timers[key]) {
+		timers[key] = [];
+	}
+	
+	if (!isArray(timers[key])) {
+		throw new Error('Timer [' + key + '] initialised with an id then called anynomously.');
+	}
+	
+	if (timers[key].length && timers[key][timers[key].length - 1].end) {
+		throw new Error('Timer [' + key + '] started again without being stopped.');
+	}
+	
+	timers[key].push(newTimer());
+	
 }
 
 function stopByIndex(timers, key) {
-
-    if (!timers[key] || !timers[key].length || timers[key][timers[key].length - 1].end) {
-        throw new Error('Timer [' + key + '] stopped without being started.');
-    }
-
-    endTimer(timers[key][timers[key].length - 1]);
-
+	
+	if (!timers[key] || !timers[key].length || timers[key][timers[key].length - 1].end) {
+		throw new Error('Timer [' + key + '] stopped without being started.');
+	}
+	
+	endTimer(timers[key][timers[key].length - 1]);
+	
 }
 
 
@@ -124,68 +124,130 @@ function stopByIndex(timers, key) {
  */
 
 function Timer() {
-
-    // Ensure a new instance has been created.
-    // Calling Timer as a function will return a new instance instead.
-    if (!(this instanceof Timer)) {
-        return new Timer();
-    }
-
-    var timers = {},
-        timersById = {};
-
-    this.start = function(key, id) {
-
-        if (id) {
-            startById(timers, key, id);
-        } else {
-            startByIndex(timers, key);
-        }
-
-        return this;
-
-    }
-
-    this.stop = function(key, id) {
-
-        if (id) {
-            stopById(timers, key, id);
-        } else {
-            stopByIndex(timers, key);
-        }
-
-        return this;
-
-    }
-
-    this.toString = this.inspect = function() {
-
-        var output = [];
-
-        each(timers, function(timer, key) {
-
-            var total = 0,
-                str = '*   ' + key + ': ';
-
-            each(timer, function(i) {
-                total += i.delta;
-            });
-
-            var times = length(timer);
-
-            if (times > 1) {
-                str += commas(times) + ' x ' + commas(Math.round(total / times)) + 'ms = ' + commas(total) + 'ms';
-            } else {
-                str += commas(total) + 'ms';
-            }
-
-            output.push(str);
-
-        });
-
-        return output.length ? 'TIMER RESULTS:\n' + output.join('\n') + '\n' : 'NO TIMERS TO OUTPUT';
-    }
+	
+	// Ensure a new instance has been created.
+	// Calling Timer as a function will return a new instance instead.
+	if (!(this instanceof Timer)) {
+		return new Timer();
+	}
+	
+	var timers = {},
+		timersById = {};
+	
+	/**
+	 * Starts a timer by key (or key + id)
+	 */
+	
+	this.start = function(key, id) {
+		
+		if (id) {
+			startById(timers, key, id);
+		} else {
+			startByIndex(timers, key);
+		}
+		
+		return this;
+		
+	}
+	
+	/**
+	 * Stops a timer by key (or key + id)
+	 */
+	this.stop = function(key, id) {
+		
+		if (id) {
+			stopById(timers, key, id);
+		} else {
+			stopByIndex(timers, key);
+		}
+		
+		return this;
+		
+	}
+	
+	/**
+	 * Formats the value of all timers as a string
+	 */
+	this.toString = 
+	this.inspect = function() {
+		
+		var output = [];
+		
+		each(timers, function(timer, key) {
+			
+			var total = 0,
+				str = '*   ' + key + ': ';
+			
+			each(timer, function(i) {
+				total += i.delta;
+			});
+			
+			var times = length(timer);
+			
+			if (times > 1) {
+				str += commas(times) + ' x ' + commas(Math.round(total / times)) + 'ms = ' + commas(total) + 'ms';
+			} else {
+				str += commas(total) + 'ms';
+			}
+			
+			output.push(str);
+			
+		});
+		
+		return output.length ? 'TIMER RESULTS:\n' + output.join('\n') + '\n' : 'NO TIMERS TO OUTPUT';
+	}
 
 }
+
+// Logs the timer output to the console
+Timer.prototype.log = function() {
+	console.log(this);
+	return this;
+}
+
+// Replaces a Timer with a Stub
+Timer.prototype.stub = function() {
+	this.isStub = true;
+	['start', 'stop', 'log', 'inspect', 'toString'].forEach(function(i) {
+		this[i] = Timer.Stub.prototype[i];
+	}, this);
+	return this;
+}
+
+/**
+ * Stub version
+ * 
+ * Provides a simple way to disable the timer and remove any overhead
+ * 
+ * Replaces the API with simple "do nothing" versions of each function
+ */
+
+Timer.Stub = function() {
+	
+	// Ensure a new instance has been created.
+	// Calling Timer.Stub as a function will return a new instance instead.
+	if (!(this instanceof Timer.Stub)) {
+		return new Timer.Stub();
+	}
+	
+	this.isStub = true;
+	
+}
+
+Timer.Stub.prototype.start = 
+Timer.Stub.prototype.stop = 
+Timer.Stub.prototype.log = function() {
+	return this;
+}
+
+Timer.Stub.prototype.inspect =
+Timer.Stub.prototype.toString = function() {
+	return '';
+}
+
+
+/**
+ * Exports the Timer class
+ */
 
 var exports = module.exports = Timer;
